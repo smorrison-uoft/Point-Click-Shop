0028578e08b73004fcd9d5a3f07e0fcd12a663d9
Duhhvonte <devonte.letts@gmail.com>
2020-09-17 18:19:13 -0400

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -15,8 +15,20 @@ connection.connect(function(err) {
     console.error("error connecting: " + err.stack);
     return;
   }
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }
+  else {
+    connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'hacktheplanet',
+      database: 'todoagain_db'
+    });
+  };
   console.log("connected as id " + connection.threadId);
 });
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
